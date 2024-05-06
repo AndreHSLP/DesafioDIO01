01cb1f544b4c6de73fe6b276d786001c727a4d91
André Henrique Silva Lima Pereira <33906283+AndreHSLP@users.noreply.github.com>
2024-05-06 16:18:33 -0300

    Inicio
    Graficos

---
+++ verificador.gd
@@ -0,0 +1,9 @@
+extends CanvasLayer
+
+@onready var nome_input:TextEdit = $MarginContainer/Panel/MarginContainer/VBoxContainer/Panel2/VBoxContainer/Panel/VBoxContainer/NomeInput
+@onready var pontos_input:TextEdit = $MarginContainer/Panel/MarginContainer/VBoxContainer/Panel2/VBoxContainer/Panel/VBoxContainer/PontosInput
+@onready var verifcar:Button = $MarginContainer/Panel/MarginContainer/VBoxContainer/Panel2/VBoxContainer/Panel/VBoxContainer/Verifcar
+
+func verificarTudo():
+	
+	pass
